a1e21b5cab5f29e9a2c8f522f994a294b39ac1df
SamikshaKature <74428521+SamikshaKature@users.noreply.github.com>
2023-01-14 13:08:14 -0500

    first commit

---
+++ Workflow/Workflow/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Workflow
+//
+//  Created by Samiksha Kature on 1/14/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("This is a test")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Workflow/Workflow/WorkflowApp.swift
@@ -0,0 +1,17 @@
+//
+//  WorkflowApp.swift
+//  Workflow
+//
+//  Created by Samiksha Kature on 1/14/23.
+//
+
+import SwiftUI
+
+@main
+struct WorkflowApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
